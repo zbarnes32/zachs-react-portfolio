e4102d0012d274f974783e500a9c514dd0c52448
Zach Barnes <zachary.barnes.32@gmail.com>
2025-02-18 18:43:12 -0700

    Reworked spacing on the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Hero = () => {
     return (
         <section id="hero" className="bg-gradient-to-r from-blue-900 to-blue-950 text-white h-screen px-6 md:px-20 flex justify-between items-center">
-            <div className="md:p-20 md:mx-8 md:basis-2/3">
+            <div className="md:mx-8 md:basis-1/2">
                 <h1 className="text-7xl font-bold pt-4 pb-2">Zachary Barnes</h1>
                 <h2 className="text-3xl py-2 md:py-0">Full Stack Developer</h2>
                 <p className="text-neutral-200 pt-6">I'm a software developer who enjoys turning ideas into functional, user-friendly web experiences. I'm passionate about learning new technologies and constantly improving my skills to become a better programmer. When I'm not coding, you'll most likely find me playing board or video games, spending quality time with family and friends, or on the golf course.</p>
